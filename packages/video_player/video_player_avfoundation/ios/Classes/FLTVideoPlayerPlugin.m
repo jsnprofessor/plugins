2bbeb4e5c3b49ff9e057e4f7abd40834244ffaf6
jason hsu <jsn_professor@hotmail.com>
2022-09-14 17:55:13 +0800

    feat: fix player disposing issue

--- packages/video_player/video_player_avfoundation/ios/Classes/FLTVideoPlayerPlugin.m
+++ packages/video_player/video_player_avfoundation/ios/Classes/FLTVideoPlayerPlugin.m
@@ -475,12 +475,6 @@ NS_INLINE CGFloat radiansToDegrees(CGFloat radians) {
   }
 }
 
-- (void)onTextureUnregistered:(NSObject<FlutterTexture> *)texture {
-  dispatch_async(dispatch_get_main_queue(), ^{
-    [self dispose];
-  });
-}
-
 - (FlutterError *_Nullable)onCancelWithArguments:(id _Nullable)arguments {
   _eventSink = nil;
   return nil;
@@ -614,22 +608,9 @@ NS_INLINE CGFloat radiansToDegrees(CGFloat radians) {
   FLTVideoPlayer *player = self.playersByTextureId[input.textureId];
   [self.registry unregisterTexture:input.textureId.intValue];
   [self.playersByTextureId removeObjectForKey:input.textureId];
-  // If the Flutter contains https://github.com/flutter/engine/pull/12695,
-  // the `player` is disposed via `onTextureUnregistered` at the right time.
-  // Without https://github.com/flutter/engine/pull/12695, there is no guarantee that the
-  // texture has completed the un-reregistration. It may leads a crash if we dispose the
-  // `player` before the texture is unregistered. We add a dispatch_after hack to make sure the
-  // texture is unregistered before we dispose the `player`.
-  //
-  // TODO(cyanglaz): Remove this dispatch block when
-  // https://github.com/flutter/flutter/commit/8159a9906095efc9af8b223f5e232cb63542ad0b is in
-  // stable And update the min flutter version of the plugin to the stable version.
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)),
-                 dispatch_get_main_queue(), ^{
-                   if (!player.disposed) {
-                     [player dispose];
-                   }
-                 });
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [player dispose];
+  });
 }
 
 - (void)setLooping:(FLTLoopingMessage *)input error:(FlutterError **)error {
